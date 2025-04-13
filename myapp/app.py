69765dbe085b6873ceaca573ca20e3cfac851a15
Oleksii Tsehelnyk <alekciy10@ukr.net>
2025-04-13 13:00:12 +0300

    2 task: prepare workflow for testing python coverage of tests

---
+++ myapp/app.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    ""Adds two numbers.""
+    return a + b
+
+def subtract(a, b):
+    ""Subtracts the second number from the first.""
+    return a - b
+
+def wrongMultiply(a, b):
+    ""Has to multiply two numbers, but to fail test does subtracting.""
+    return subtract(a, b)
+
+def increment(a):
+    ""Returns incremented result.""
+    return a + 1
