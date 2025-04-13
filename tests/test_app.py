7cc92bae86add8a8d01940123768e769e63887e2
Oleksii Tsehelnyk <alekciy10@ukr.net>
2025-04-13 20:31:22 +0300

    replace assert with pytest_check

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,19 @@
 from myapp.app import add, subtract, wrongMultiply, increment
 import pytest
+import pytest_check as check
 
 def test_add():
     """Tests the additional function."""
-    assert add(2, 3) == 5
+    check.equal(add(2, 3), 5)
 
 def test_subtract():
     """Tests the subtraction function."""
-    assert subtract(5, 3) == 2
+    check.equal(subtract(5, 3), 2)
 
 def test_multiply():
     """Tests the multiply function."""
-    assert wrongMultiply(5, 3) == 15
+    check.equal(wrongMultiply(5, 3), 15)
 
 def test_increment():
     """Tests the subtraction function."""
-    assert increment(5) == 5 + 1
+    check.equal(increment(5), 5 + 1)
