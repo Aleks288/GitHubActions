a7d2204d4948925c22940e557c2bb25c662dab3b
Oleksii Tsehelnyk <alekciy10@ukr.net>
2025-04-13 20:39:51 +0300

    try to write correct wrong example

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,19 @@
 from myapp.app import add, subtract, wrongMultiply, increment
 import pytest
-import pytest_check as check
 
 def test_add():
     """Tests the additional function."""
-    check.equal(add(2, 3), 5)
+    assert add(2, 3) == 5
 
 def test_subtract():
     """Tests the subtraction function."""
-    check.equal(subtract(5, 3), 2)
+    assert subtract(5, 3) == 2
 
+@pytest.mark.xfail
 def test_multiply():
     """Tests the multiply function."""
-    check.equal(wrongMultiply(5, 3), 15)
+    assert wrongMultiply(5, 3) == 15
 
 def test_increment():
     """Tests the subtraction function."""
-    check.equal(increment(5), 5 + 1)
+    assert increment(5) == 5 + 1
