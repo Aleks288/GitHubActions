44b30ec91cb9b2d6e4ee4ab6df3d99f54c86c425
Oleksii Tsehelnyk <alekciy10@ukr.net>
2025-04-13 13:44:42 +0300

    fix quotes in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,17 +2,17 @@ from myapp.app import add, subtract, wrongMultiply, increment
 import pytest
 
 def test_add():
-    ""Tests the additional function.""
+    """Tests the additional function.""""
     assert add(2, 3) == 5
 
 def test_subtract():
-    ""Tests the subtraction function.""
+    """Tests the subtraction function.""""
     assert subtract(5, 3) == 2
 
 def test_multiply():
-    ""Tests the multiply function.""
+    """Tests the multiply function.""""
     assert wrongMultiply(5, 3) == 15
 
 def test_increment():
-    ""Tests the subtraction function.""
+    """Tests the subtraction function.""""
     assert increment(a) == a + 1
