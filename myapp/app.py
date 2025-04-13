8886b5a1a2733a71eab2d851d75f2a42519fee35
Oleksii Tsehelnyk <alekciy10@ukr.net>
2025-04-13 13:46:48 +0300

    fix quotes in app.py

--- myapp/app.py
+++ myapp/app.py
@@ -1,15 +1,15 @@
 def add(a, b):
-    ""Adds two numbers.""
+    """Adds two numbers."""
     return a + b
 
 def subtract(a, b):
-    ""Subtracts the second number from the first.""
+    """Subtracts the second number from the first."""
     return a - b
 
 def wrongMultiply(a, b):
-    ""Has to multiply two numbers, but to fail test does subtracting.""
+    """Has to multiply two numbers, but to fail test does subtracting."""
     return subtract(a, b)
 
 def increment(a):
-    ""Returns incremented result.""
+    """Returns incremented result."""
     return a + 1
